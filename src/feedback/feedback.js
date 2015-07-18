d05576fed9f8ef70d0a21c3fd5f77a89c2aa8c1b
Trevor D. Miller <trevordmiller@icloud.com>
2015-07-18 14:35:17 -0600

    Preparing for screencast demo.

--- src/feedback/feedback.js
+++ src/feedback/feedback.js
@@ -1,9 +1,5 @@
-import inputStorage from '../input-storage/input-storage';
-
 let feedback = {
 	init() {
-		inputStorage.restore('userFeedback', '.feedback__textarea');
-		inputStorage.save('userFeedback', '.feedback__textarea');
 	}
 };
 
